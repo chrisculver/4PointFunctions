fc27ff4c3a9079f4abf7fbb3b11849816a21cde2
chrisculver <chris.cb.culver@gmail.com>
2021-11-11 14:50:56 +0000

    Added isospin sym continuum notebook, also print latex format right in the notebook

--- print_utilities.py
+++ print_utilities.py
@@ -9,6 +9,42 @@ def platexStr(p):
     return p.name + '(' + p.ti.replace('t','x') + p.ti + '\mid ' + p.tf.replace('t','x') + p.tf + ')' + '_{\\substack{' + p.left_indices.s + '\\\\' + p.left_indices.c + '}' +  '\\substack{' + p.right_indices.s + '\\\\' + p.right_indices.c + '}}'
     
 
+def diagram_as_latex_str(d,i):
+    #if(i==1 or i==6):
+    s=''
+    tmp='d_{'+str(i)+'}='+str(d.coef) + ' '
+    for p in d.props:
+        tmp+=platexStr(p)
+    for c in d.commuting:
+        tmp+=latexStr(c)
+    s+=tmp+'\\\\'
+    return s
+
+def labelled_diagram_as_latex_str(d,i,label):
+    s=''
+    tmp='d_{'+str(i)+'}^{\\text{('+label+')}}='+str(d.coef) + ' '
+    for p in d.props:
+        tmp+=platexStr(p)
+    for c in d.commuting:
+        tmp+=latexStr(c)
+    s+=tmp+'\\\\'
+    return s
+    
+    
+    
+def print_diagrams_labelled(diags,sortd):
+    print('\\beqs')
+    for i,label in sortd.items():
+        #if(i==1 or i==6):
+        tmp='d_{'+str(i)+'}^{\\text{('+label+')}}='+str(diags[i].coef) + ' '
+        for p in diags[i].props:
+            tmp+=platexStr(p)
+        for c in diags[i].commuting:
+            tmp+=latexStr(c)
+        print(tmp+'\\\\')
+    print('\\eeqs')
+    
+    
 def print_diagrams(total_diags):
     print('\\beqs')
     for i,d in enumerate(total_diags):
@@ -19,20 +55,4 @@ def print_diagrams(total_diags):
         for c in d.commuting:
             tmp+=latexStr(c)
         print(tmp+'\\\\')
-    print('\\eeqs')
-    
-    
-    
-def print_diagrams_labelled(diags,sortd):
-    print('\\beqs')
-    newI=0
-    for i,label in sortd.items():
-        #if(i==1 or i==6):
-        tmp='d_{'+str(i)+'}^{\\text{('+label+')}}='+str(diags[i].coef) + ' '
-        for p in diags[i].props:
-            tmp+=platexStr(p)
-        for c in diags[i].commuting:
-            tmp+=latexStr(c)
-        print(tmp+'\\\\')
-        newI+=1
     print('\\eeqs')
